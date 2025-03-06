768ad4f64eac86d81cce1d54d5f863accc1bdcba
datboisus <donvu251106@gmail.com>
2025-03-06 10:17:40 -0800

    1

--- Main.java
+++ Main.java
@@ -4,17 +4,41 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // Test code here
+   int [][] mat = productTable (0,0);
+   for (int[] row : main)
+   {
+    for (int elem :)
+   }
   }
 
 
-  public static int sumOfDiag(int[][] matrix)
+  public static int sumOfDiag(int[][] mat)
   {
-    return 0;
+    int sum = 0;
+    for (int row = 0; row < mat.length; row++)
+    {
+      for (int col = 0; col < mat[row].length; col++)
+      {
+        if (row == col)
+        {
+          sum += mat[row][col];
+        }
+      }
+    }
+    
+    return sum;
   }
 
   public static int[][] productTable(final int numRows, final int numCols)
   {
-    return null;
+    int [][] mat = new int [numRows][numCols];
+    for (int r = 0; r < numRows; r++)
+    {
+      for (int col = 0; c < numCols; c++)
+      {
+        mat[r][c] = r * c;
+      }
+    }
+    return mat;
   }
 }
